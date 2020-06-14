fd76a13fb003f175c290dcd3d871137c2bd577ca
Hari Rathor <hari.rathor@mindtree.com>
2020-06-14 11:42:55 +0400

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -24,8 +24,8 @@ render() {
                 </section>
                 <section className="buttons">
                 <ul>
-                    {this.props.quiz_question.answer_options.map((value, index) => {
-                        return <QuizQuestionButton key={index} button_text={value} 
+                    {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                        return <QuizQuestionButton key={index} button_text={answer_option} 
                         clickHandler = {this.handleClick.bind(this)} />
                     })}
                 </ul>
